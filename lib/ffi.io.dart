c0be71525458b5382d52cf4733e805acad913e85
Sander in 't Hout <sander@inthout.me>
2023-03-07 22:28:45 +0100

    Upgrade Rust packages, regenerate bridge_definitions, fix dylib loading on macOS

--- lib/ffi.io.dart
+++ lib/ffi.io.dart
@@ -1,8 +1,11 @@
+import 'dart:ffi';
 import 'dart:io';
-import 'package:flutter_rust_bridge/flutter_rust_bridge.dart';
-import 'bridge_generated.io.dart';
+import 'package:flutter_rust_bridge_example/bridge_definitions.dart';
 
 const base = 'flutter_rust_bridge_example';
 final path = Platform.isWindows ? '$base.dll' : 'lib$base.so';
-late final dylib = loadDylib(path);
-late final api = FlutterRustBridgeExampleImpl(dylib);
+final dylib = Platform.isIOS || Platform.isMacOS
+    ? DynamicLibrary.executable()
+    : DynamicLibrary.open(path);
+
+final api = FlutterRustBridgeExampleImpl(dylib);
