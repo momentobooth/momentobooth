b5aeca56e4e0af46de73730fdc8defc03835db2b
Sander in 't Hout <sander@inthout.me>
2023-03-18 17:29:03 +0100

    Upgrade more dependencies, fix macOS release binary by fixing static library strip issue

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -4,7 +4,14 @@ import FlutterMacOS
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-    dummy_method_to_enforce_bundling()
+    // See: https://cjycode.com/flutter_rust_bridge/integrate/ios_headers.html
+    let dummyReturnValue = dummy_method_to_enforce_bundling()
+    doNothing(with: dummyReturnValue)
     return true
+  }
+    
+  @inline(never)
+  func doNothing(with object: Any) {
+    // This method does nothing with the object parameter
   }
 }
