a6080cbfa81d32e355850b70eb64cbfd7389e3d9
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-04-03 00:26:09 +0200

    Make home directory dynamic.
    As stated, it should actually be a setting instead.

--- lib/views/capture_screen/capture_screen_controller.dart
+++ lib/views/capture_screen/capture_screen_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:io' show Platform, stdout;
+
 import 'package:flutter_rust_bridge_example/model/photo_state.dart';
 import 'package:flutter_rust_bridge_example/utils/sony_remote_photo_capture.dart';
 import 'package:flutter_rust_bridge_example/views/base/screen_controller_base.dart';
@@ -13,7 +15,16 @@ class CaptureScreenController extends ScreenControllerBase<CaptureScreenViewMode
   });
 
   void captureAndGetPhoto() async {
-    final capturer = SonyRemotePhotoCapture(r"C:\Users\caspe\Pictures");
+    // Fixme: This should be a setting
+    String os = Platform.operatingSystem;
+    String home = "";
+    Map<String, String> envVars = Platform.environment;
+    if (Platform.isMacOS || Platform.isLinux) {
+      home = envVars['HOME']!;
+    } else if (Platform.isWindows) {
+      home = envVars['UserProfile']!;
+    }
+    final capturer = SonyRemotePhotoCapture("$home\\Pictures");
     final image = await capturer.captureAndGetPhoto();
     PhotoStateBase.instance.photos.add(image);
   }
