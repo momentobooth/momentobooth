cf6854843d66751f6fc35986090ddf5af3e7c168
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-04-04 00:18:41 +0200

    Actually use capture method setting.

--- lib/views/capture_screen/capture_screen_view_model.dart
+++ lib/views/capture_screen/capture_screen_view_model.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 import 'package:flutter/animation.dart';
 import 'package:flutter_rust_bridge_example/managers/photos_manager.dart';
 import 'package:flutter_rust_bridge_example/managers/settings_manager.dart';
-import 'package:flutter_rust_bridge_example/utils/capture_method.dart';
+import 'package:flutter_rust_bridge_example/utils/capture_method.dart' as x;
+import 'package:flutter_rust_bridge_example/utils/fake_capture_method.dart';
 import 'package:flutter_rust_bridge_example/utils/sony_remote_photo_capture.dart';
 import 'package:flutter_rust_bridge_example/views/base/screen_view_model_base.dart';
+import 'package:flutter_rust_bridge_example/models/settings.dart';
 import 'package:mobx/mobx.dart';
 
 part 'capture_screen_view_model.g.dart';
@@ -14,7 +16,7 @@ class CaptureScreenViewModel = CaptureScreenViewModelBase with _$CaptureScreenVi
 
 abstract class CaptureScreenViewModelBase extends ScreenViewModelBase with Store {
 
-  late final CaptureMethod capturer;
+  late final x.CaptureMethod capturer;
 
   int get counterStart => SettingsManagerBase.instance.settings.captureDelaySeconds;
 
@@ -45,7 +47,11 @@ abstract class CaptureScreenViewModelBase extends ScreenViewModelBase with Store
       home = envVars['UserProfile']!;
     }
 
-    capturer = SonyRemotePhotoCapture("$home\\Pictures");
+    if (SettingsManagerBase.instance.settings.hardware.captureMethod == CaptureMethod.sonyImagingEdgeDesktop){
+      capturer = SonyRemotePhotoCapture("$home\\Pictures");
+  	} else {
+      capturer = FakeCaptureMethod();
+    }
     Future.delayed(photoDelay).then((_) => captureAndGetPhoto());
   }
 
