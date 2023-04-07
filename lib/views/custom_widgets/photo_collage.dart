40f7eb6ddeb4fa8399b7d6600f0e1f3ee3465c4a
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-04-07 16:02:05 +0200

    Provide layouts for 1, 2, 3, and 4 photos.

--- lib/views/custom_widgets/photo_collage.dart
+++ lib/views/custom_widgets/photo_collage.dart
@@ -2,6 +2,10 @@ import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
 
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_rust_bridge_example/managers/photos_manager.dart';
+import 'package:flutter_rust_bridge_example/theme/momento_booth_theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
@@ -27,6 +31,8 @@ class PhotoCollageState extends State<PhotoCollage> {
 
   final GlobalKey _globalKey = GlobalKey();
 
+  MomentoBoothThemeData get theme => MomentoBoothThemeData.defaults();
+
   @override
   Widget build(BuildContext context) {
     return RepaintBoundary(
@@ -34,30 +40,114 @@ class PhotoCollageState extends State<PhotoCollage> {
       child: Align(
         child: AspectRatio(
           aspectRatio: widget.aspectRatio,
-          child: LayoutGrid(
-            areas: '''
-                header1 header2
-                content1 content2
-                content3 content4
-                content5 content6
-              ''',
-            rowSizes: [auto, auto, auto, auto],
-            columnSizes: [1.fr, 1.fr],
-            columnGap: 12,
-            rowGap: 12,
-            children: [
-              Center(child: Text("Powered by Casper die echt teringsnel Flutter geleerd heeft")).inGridArea('header1'),
-              Center(child: SvgPicture.asset("assets/svg/logo.svg", color: Colors.white)).inGridArea('header2'),
-              Center(child: Image.asset("assets/bitmap/sample-background.jpg")).inGridArea('content1'),
-              Center(child: Image.asset("assets/bitmap/sample-background.jpg")).inGridArea('content2'),
-              Center(child: Image.asset("assets/bitmap/sample-background.jpg")).inGridArea('content3'),
-              Center(child: Image.asset("assets/bitmap/sample-background.jpg")).inGridArea('content4'),
-              Center(child: Image.asset("assets/bitmap/sample-background.jpg")).inGridArea('content5'),
-              Center(child: Image.asset("assets/bitmap/sample-background.jpg")).inGridArea('content6'),
-            ],
-          ),
+          child: Observer(builder: (BuildContext context) { return _layout; },)
         ),
       ),
+    );
+  }
+
+  Widget get _layout {
+    if (PhotosManagerBase.instance.chosen.isEmpty) {
+      return _zeroLayout;
+    } else if (PhotosManagerBase.instance.chosen.length == 1) {
+      return _oneLayout;
+    } else if (PhotosManagerBase.instance.chosen.length == 2) {
+      return _twoLayout;
+    } else if (PhotosManagerBase.instance.chosen.length == 3) {
+      return _threeLayout;
+    } else if (PhotosManagerBase.instance.chosen.length == 4) {
+      return _fourLayout;
+    }
+    return Container();
+  }
+
+  Widget get _zeroLayout {
+    return Center(child: AutoSizeText("Select some photos :)", style: theme.titleStyle,),);
+  }
+
+  Widget get _oneLayout {
+    return LayoutGrid(
+      areas: '''
+          header
+          content
+        ''',
+      rowSizes: [1.fr, 6.fr],
+      columnSizes: [1.fr],
+      columnGap: 12,
+      rowGap: 12,
+      children: [
+        Center(child: SvgPicture.asset("assets/svg/logo.svg", color: Colors.white)).inGridArea('header'),
+        Center(child: RotatedBox(
+          quarterTurns: 1,
+          child: Image.memory(PhotosManagerBase.instance.photos[0])),
+        ).inGridArea('content'),
+      ],
+    );
+  }
+
+  Widget get _twoLayout {
+    return LayoutGrid(
+      areas: '''
+          header
+          content1
+          content2
+        ''',
+      rowSizes: [1.fr, 3.fr, 3.fr],
+      columnSizes: [1.fr],
+      columnGap: 12,
+      rowGap: 12,
+      children: [
+        Center(child: SvgPicture.asset("assets/svg/logo.svg", color: Colors.white)).inGridArea('header'),
+        for (int i = 0; i < PhotosManagerBase.instance.photos.length; i++) ...[
+          Center(child: Image.memory(PhotosManagerBase.instance.photos[i])).inGridArea('content${i+1}'),
+        ]
+      ],
+    );
+  }
+
+  Widget get _threeLayout {
+    return LayoutGrid(
+      areas: '''
+          header1 header2
+          content1 content4
+          content2 content5
+          content3 content6
+        ''',
+      rowSizes: [auto, auto, auto, auto],
+      columnSizes: [1.fr, 1.fr],
+      columnGap: 12,
+      rowGap: 12,
+      children: [
+        Center(child: Text("Powered by Casper die echt teringsnel Flutter geleerd heeft")).inGridArea('header1'),
+        Center(child: SvgPicture.asset("assets/svg/logo.svg", color: Colors.white)).inGridArea('header2'),
+        for (int i = 0; i < PhotosManagerBase.instance.photos.length; i++) ...[
+          Center(child: Image.memory(PhotosManagerBase.instance.photos[i])).inGridArea('content${i+1}'),
+          Center(child: Image.memory(PhotosManagerBase.instance.photos[i])).inGridArea('content${i+4}'),
+        ]
+      ],
+    );
+  }
+
+  Widget get _fourLayout {
+    return LayoutGrid(
+      areas: '''
+          content1 content2
+          header   header
+          content3 content4
+        ''',
+      rowSizes: [3.fr, 1.fr, 3.fr],
+      columnSizes: [1.fr, 1.fr],
+      columnGap: 12,
+      rowGap: 12,
+      children: [
+        Center(child: SvgPicture.asset("assets/svg/logo.svg", color: Colors.white)).inGridArea('header'),
+        for (int i = 0; i < PhotosManagerBase.instance.photos.length; i++) ...[
+          Center(child: RotatedBox(
+            quarterTurns: 1,
+            child: Image.memory(PhotosManagerBase.instance.photos[i])),
+          ).inGridArea('content${i+1}'),
+        ]
+      ],
     );
   }
 
