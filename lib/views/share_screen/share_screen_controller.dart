41b514d3ad71b30096062da225b522bb6c1cb4eb
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-04-08 00:36:27 +0200

    Attempt to print a photo.

--- lib/views/share_screen/share_screen_controller.dart
+++ lib/views/share_screen/share_screen_controller.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_rust_bridge_example/managers/photos_manager.dart';
+import 'package:flutter_rust_bridge_example/managers/settings_manager.dart';
 import 'package:flutter_rust_bridge_example/views/base/screen_controller_base.dart';
 import 'package:flutter_rust_bridge_example/views/share_screen/share_screen_view_model.dart';
+import 'package:printing/printing.dart';
 
 class ShareScreenController extends ScreenControllerBase<ShareScreenViewModel> {
 
@@ -18,8 +21,26 @@ class ShareScreenController extends ScreenControllerBase<ShareScreenViewModel> {
     print("Requesting QR code");
   }
 
-  void onClickPrint() {
+  Future<void> onClickPrint() async {
     print("Requesting photo print");
+    // Find printer that was set in settings in available printers.
+    final printers = await Printing.listPrinters();
+    Printer? selected;
+    for (var printer in printers) {
+      if (printer.name == SettingsManagerBase.instance.settings.hardware.printerName) {
+        selected = printer;
+        break;
+      }
+    }
+    if (selected == null) {
+      print("Could not find set printer");
+      return;
+    }
+
+    // Get photo and print it.
+    final photoToPrint = PhotosManagerBase.instance.photos.first;
+    await Printing.directPrintPdf(
+        printer: selected, name: "MomentoBooth image", onLayout: (_) => photoToPrint);
   }
 
 }
