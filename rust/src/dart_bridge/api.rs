eb0eeb36b36b17ef7ef1d1d90884572e23681947
Sander in 't Hout <sander@inthout.me>
2023-04-10 21:49:48 +0200

    Add ffsend client to Rust library, make it available to Dart

--- rust/src/dart_bridge/api.rs
+++ rust/src/dart_bridge/api.rs
@@ -1,6 +1,6 @@
 use flutter_rust_bridge::StreamSink;
 
-use crate::{hardware_control::live_view::nokhwa::{self, NokhwaCameraInfo}, LogEvent, HardwareInitializationFinishedEvent};
+use crate::{hardware_control::live_view::nokhwa::{self, NokhwaCameraInfo}, utils::ffsend_client::{self, FfSendTransferProgress}, LogEvent, HardwareInitializationFinishedEvent};
 
 pub fn initialize_log(log_sink: StreamSink<LogEvent>) {
     crate::initialize_log(log_sink);
@@ -12,4 +12,12 @@ pub fn initialize_hardware(ready_sink: StreamSink<HardwareInitializationFinished
 
 pub fn nokhwa_get_cameras() -> Vec<NokhwaCameraInfo> {
     return nokhwa::get_cameras();
+}
+
+// ////// //
+// ffsend //
+// ////// //
+
+pub fn ffsend_upload_file(host_url: String, file_path: String, download_filename: Option<String>, update_sink: StreamSink<FfSendTransferProgress>) {
+    ffsend_client::upload_file(host_url, file_path, download_filename, update_sink)
 }
