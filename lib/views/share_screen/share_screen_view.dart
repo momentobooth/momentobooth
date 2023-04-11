8093f659e46bb7730ab58be6f17c3a10ef719c72
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-04-11 16:17:15 +0200

    Create SliderWidget to show QR code when necessary.

--- lib/views/share_screen/share_screen_view.dart
+++ lib/views/share_screen/share_screen_view.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_rust_bridge_example/extensions/build_context_extension.dart';
 import 'package:flutter_rust_bridge_example/theme/momento_booth_theme_data.dart';
 import 'package:flutter_rust_bridge_example/views/base/screen_view_base.dart';
 import 'package:flutter_rust_bridge_example/views/custom_widgets/wrappers/sample_background.dart';
@@ -121,28 +122,79 @@ class ShareScreenView extends ScreenViewBase<ShareScreenViewModel, ShareScreenCo
   }
 
   Widget get _qrCode {
-    return Observer(builder: (context) =>
-      Padding(
-        padding: EdgeInsets.all(30),
-        child: Center(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Color(0xffffffff),
-              borderRadius: BorderRadius.circular(10),
-              border: theme.captureCounterContainerBorder,
-              boxShadow: [theme.captureCounterContainerShadow],
-            ),
-            child: PrettyQr(
-              size: 500,
-              data: viewModel.qrUrl,
-              errorCorrectLevel: QrErrorCorrectLevel.L,
-              roundEdges: true,
-            ),
+    return Observer(builder: (context) {
+      if (viewModel.uploadState != UploadState.done) {
+        return SizedBox();
+      }
+      return SliderWidget(
+        child: Container(
+          padding: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Color(0xffffffff),
+            borderRadius: BorderRadius.circular(10),
+            border: theme.captureCounterContainerBorder,
+            boxShadow: [theme.captureCounterContainerShadow],
+          ),
+          child: PrettyQr(
+            size: 500,
+            data: viewModel.qrUrl,
+            errorCorrectLevel: QrErrorCorrectLevel.L,
+            roundEdges: true,
+          ),
+        ),
+      );
+    });
+  }
+
+}
+
+class SliderWidget extends StatefulWidget {
+  final Widget child; 
+
+  const SliderWidget({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  State<SliderWidget> createState() => _SliderWidgetState();
+}
+
+class _SliderWidgetState extends State<SliderWidget>
+    with SingleTickerProviderStateMixin {
+
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 600),
+    vsync: this,
+  )..forward();
+  
+  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
+    begin: const Offset(0.0, 1.5),
+    end: Offset.zero,
+  ).animate(CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeOut,
+  ));
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _offsetAnimation,
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Padding(
+          padding: EdgeInsets.all(30),
+          child: Center(
+            child: widget.child,
           ),
         ),
       ),
     );
   }
-
 }
