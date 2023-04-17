e8576f06605bcdb359b084290a414c79cca80e60
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-04-18 00:29:53 +0200

    Remove superfluous reset.

--- lib/views/start_screen/start_screen_view_model.dart
+++ lib/views/start_screen/start_screen_view_model.dart
@@ -9,6 +9,8 @@ class StartScreenViewModel = StartScreenViewModelBase with _$StartScreenViewMode
 abstract class StartScreenViewModelBase extends ScreenViewModelBase with Store {
 
   StartScreenViewModelBase({required super.contextAccessor}) {
+    // Remove images in memory
+    // Fixme: maybe somewhere else is nicer, but for now it's here.
     PhotosManagerBase.instance.reset();
   }
 
