510635b959e61045c14c42ecaa98454b24a79fd8
Sander in 't Hout <sander@inthout.me>
2023-04-24 00:03:10 +0200

    Improve animations between screens

--- lib/views/base/fade_transition_page.dart
+++ lib/views/base/fade_transition_page.dart
@@ -3,8 +3,6 @@ import 'package:go_router/go_router.dart';
 
 class FadeTransitionPage extends CustomTransitionPage<void> {
 
-  static final CurveTween _curveTween = CurveTween(curve: Curves.easeIn);
-
   /// Creates a [FadeTransitionPage].
   FadeTransitionPage({
     required LocalKey super.key,
@@ -12,8 +10,17 @@ class FadeTransitionPage extends CustomTransitionPage<void> {
   }) : super(
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
             return FadeTransition(
-              opacity: animation.drive(_curveTween),
-              child: child,
+              opacity: Tween<double>(begin: 0.0, end: 1.0).animate(animation),
+              child: ScaleTransition(
+                scale: Tween<double>(begin: 0.95, end: 1.0).animate(animation),
+                child: FadeTransition(
+                  opacity: Tween<double>(begin: 1.0, end: 0.0).animate(secondaryAnimation),
+                  child: ScaleTransition(
+                    scale: Tween<double>(begin: 1.0, end: 1.3).animate(secondaryAnimation),
+                    child: child,
+                  ),
+                ),
+              ),
             );
           },
         );
