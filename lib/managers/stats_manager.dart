5bd6850f21bc7084807ae67857295c159d03892d
Sander in 't Hout <sander@inthout.me>
2023-04-29 17:17:01 +0200

    Update Stats model with default, fix load/save of stats, add timer

--- lib/managers/stats_manager.dart
+++ lib/managers/stats_manager.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:loggy/loggy.dart';
@@ -5,6 +6,7 @@ import 'package:mobx/mobx.dart';
 import 'package:momento_booth/models/stats.dart';
 import 'package:path/path.dart' hide context;
 import 'package:path_provider/path_provider.dart';
+import 'package:synchronized/synchronized.dart';
 import 'package:toml/toml.dart';
 
 part 'stats_manager.g.dart';
@@ -24,11 +26,8 @@ enum StatFields {
 
 abstract class StatsManagerBase with Store, UiLoggy {
 
-  @observable
-  Stats? _stats;
-
-  @computed
-  Stats get stats => _stats!;
+  @readonly
+  Stats _stats = Stats();
 
   // ////////////// //
   // Initialization //
@@ -43,35 +42,37 @@ abstract class StatsManagerBase with Store, UiLoggy {
   // /////// //
 
   @action
-  void addTap() { _stats = _stats!.copyWith(taps: _stats!.taps+1); }
+  void addTap() { _stats = _stats.copyWith(taps: _stats.taps+1); }
 
   @action
-  void addLiveViewFrame() { _stats = _stats!.copyWith(liveViewFrames: _stats!.liveViewFrames+1); }
+  void addLiveViewFrame() { _stats = _stats.copyWith(liveViewFrames: _stats.liveViewFrames+1); }
 
   @action
-  void addPrintedPhoto() { _stats = _stats!.copyWith(printedPhotos: _stats!.printedPhotos+1); }
+  void addPrintedPhoto() { _stats = _stats.copyWith(printedPhotos: _stats.printedPhotos+1); }
 
   @action
-  void addUploadedPhoto() { _stats = _stats!.copyWith(uploadedPhotos: _stats!.uploadedPhotos+1); }
+  void addUploadedPhoto() { _stats = _stats.copyWith(uploadedPhotos: _stats.uploadedPhotos+1); }
 
   @action
-  void addCapturedPhoto() { _stats = _stats!.copyWith(capturedPhotos: _stats!.capturedPhotos+1); }
+  void addCapturedPhoto() { _stats = _stats.copyWith(capturedPhotos: _stats.capturedPhotos+1); }
 
   @action
-  void addCreatedSinglePhoto() { _stats = _stats!.copyWith(createdSinglePhotos: _stats!.createdSinglePhotos+1); }
+  void addCreatedSinglePhoto() { _stats = _stats.copyWith(createdSinglePhotos: _stats.createdSinglePhotos+1); }
 
   @action
-  void addRetake() { _stats = _stats!.copyWith(retakes: _stats!.retakes+1); }
+  void addRetake() { _stats = _stats.copyWith(retakes: _stats.retakes+1); }
 
   @action
-  void addCreatedMultiCapturePhoto() { _stats = _stats!.copyWith(createdMultiCapturePhotos: _stats!.createdMultiCapturePhotos+1); }
+  void addCreatedMultiCapturePhoto() { _stats = _stats.copyWith(createdMultiCapturePhotos: _stats.createdMultiCapturePhotos+1); }
   
   // /////////// //
   // Persistence //
   // /////////// //
 
   late File _statsFile;
-  static const _fileName = "MomentoBooth_Statistics.toml";
+  static const _fileName = "Statistics.toml";
+  static const _statsSaveTimerInterval = Duration(minutes: 1);
+  static final _stateSaveLock = Lock();
 
   @action
   Future<void> load() async {
@@ -79,34 +80,39 @@ abstract class StatsManagerBase with Store, UiLoggy {
     await _ensureStatsFileIsSet();
 
     if (!await _statsFile.exists()) {
-      // File does not exist, create new statistics file
-      await _save();
-      return;
+      // File does not exist
+      _stats = Stats();
+      loggy.warning("Persisted statistics file not found"); 
+    } else {
+      // File does exist
+      loggy.debug("Loading persisted statistics");
+      try {
+        String statsAsToml = await _statsFile.readAsString();
+        TomlDocument statsDocument = TomlDocument.parse(statsAsToml);
+        Map<String, dynamic> statsMap = statsDocument.toMap();
+        _stats = Stats.fromJson(statsMap);
+        loggy.debug("Loaded persisted statistics");
+      } catch (_) {
+        // Fixme: Failed to parse, ignore for now
+        loggy.warning("Persisted statistics could not be loaded"); 
+      }
     }
 
-    // File does exist
-    String statisticsAsToml = await _statsFile.readAsString();
-    TomlDocument statisticsDocument = TomlDocument.parse(statisticsAsToml);
-    try {
-      var mapWithStringKey = statisticsDocument.toMap().map((key, value) => MapEntry(key, value as int));
-      stats =  as ObservableMap<StatFields, int>;
-    } catch (_) {
-      // Fixme: Failed to parse
-      return;
-    }
-    loggy.debug("Loaded statistics: $stats");
+    Timer.periodic(_statsSaveTimerInterval, (timer) => _save());
   }
 
   Future<void> _save() async {
-    loggy.debug("Saving statistics");
-    await _ensureStatsFileIsSet();
+    await _stateSaveLock.synchronized(() async {
+      loggy.debug("Saving statistics");
+      await _ensureStatsFileIsSet();
 
-    var mapWithStringKey = stats.map((key, value) => MapEntry(key.toString(), value));
-    TomlDocument statisticsDocument = TomlDocument.fromMap(mapWithStringKey);
-    String statisticsAsToml = statisticsDocument.toString();
-    _statsFile.writeAsString(statisticsAsToml);
+      Map<String, dynamic> mapWithStringKey = _stats.toJson();
+      TomlDocument statsDocument = TomlDocument.fromMap(mapWithStringKey);
+      String statsAsToml = statsDocument.toString();
+      await _statsFile.writeAsString(statsAsToml);
 
-    loggy.debug("Saved statistics");
+      loggy.debug("Saved statistics");
+    });
   }
 
   // /////// //
--- lib/models/stats.dart
+++ lib/models/stats.dart
@@ -14,14 +14,14 @@ class Stats with _$Stats implements TomlEncodableValue {
   const Stats._();
 
   const factory Stats({
-    required int taps,
-    required int liveViewFrames,
-    required int printedPhotos,
-    required int uploadedPhotos,
-    required int capturedPhotos,
-    required int createdSinglePhotos,
-    required int retakes,
-    required int createdMultiCapturePhotos,
+    @Default(0) int taps,
+    @Default(0) int liveViewFrames,
+    @Default(0) int printedPhotos,
+    @Default(0) int uploadedPhotos,
+    @Default(0) int capturedPhotos,
+    @Default(0) int createdSinglePhotos,
+    @Default(0) int retakes,
+    @Default(0) int createdMultiCapturePhotos,
   }) = _Stats;
 
   factory Stats.fromJson(Map<String, Object?> json) => _$StatsFromJson(json);
