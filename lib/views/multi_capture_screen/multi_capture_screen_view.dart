c45a595bc5ce559469b6928507675ddd088d383d
Sander in 't Hout <sander@inthout.me>
2023-06-06 02:11:08 +0200

    Cleanup Multi Capture Screen code

--- lib/views/multi_capture_screen/multi_capture_screen_view.dart
+++ lib/views/multi_capture_screen/multi_capture_screen_view.dart
@@ -28,80 +28,92 @@ class MultiCaptureScreenView extends ScreenViewBase<MultiCaptureScreenViewModel,
           children: [
             Flexible(
               flex: 1,
-              child: Column(
-                children: [
-                  Flexible(child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: AutoSizeText("Photo ${viewModel.photoNumber}/${viewModel.maxPhotos}", style: theme.titleStyle, maxLines: 1,),
-                  )),
-                  for (int i = 0; i < PhotosManagerBase.instance.photos.length; i++)
-                    Flexible(
-                      flex: 0,
-                      child: Padding(padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: AspectRatio(
-                          aspectRatio: 1.5,
-                          child: ImageWithLoaderFallback.memory(PhotosManagerBase.instance.photos[i]),
-                        ),
-                      ),
-                    ),
-                  for (int i = PhotosManagerBase.instance.photos.length; i < 4; i++)
-                    Flexible(
-                      flex: 0,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: AspectRatio(
-                          aspectRatio: 1.5,
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(vertical: 10),
-                            decoration: BoxDecoration(
-                              border: theme.captureCounterContainerBorder,
-                              // boxShadow: [theme.captureCounterContainerShadow],
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                ],
-              ),
+              child: _photoColumn,
             ),
             Flexible(
               flex: 5,
               child: AspectRatio(
                 aspectRatio: 1.5,
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    const LiveView(),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        _getReadyText,
-                        Flexible(
-                          child: Container(
-                            padding: const EdgeInsets.all(40.0),
-                            constraints: const BoxConstraints(maxWidth: 600, maxHeight: 600),
-                            child: Observer(builder: (_) {
-                              return AnimatedOpacity(
-                                duration: const Duration(milliseconds: 50),
-                                opacity: viewModel.showCounter ? 1.0 : 0.0,
-                                child: CaptureCounter(
-                                  onCounterFinished: viewModel.onCounterFinished,
-                                  counterStart: viewModel.counterStart,
-                                ),
-                              );
-                            }),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                child: _liveViewWithCounter,
               ),
             ),
           ],
         ),
-        _flashAnimation
+        _flashAnimation,
       ],
+    );
+  }
+
+  Widget get _photoColumn {
+    return Column(
+      children: [
+        Flexible(child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: AutoSizeText(
+            "Photo ${viewModel.photoNumber}/${viewModel.maxPhotos}",
+            style: theme.titleStyle,
+            maxLines: 1,
+          ),
+        )),
+        for (int i = 0; i < PhotosManagerBase.instance.photos.length; i++)
+          Flexible(
+            flex: 0,
+            child: Padding(padding: const EdgeInsets.symmetric(vertical: 10),
+              child: AspectRatio(
+                aspectRatio: 1.5,
+                child: ImageWithLoaderFallback.memory(PhotosManagerBase.instance.photos[i]),
+              ),
+            ),
+          ),
+        for (int i = PhotosManagerBase.instance.photos.length; i < 4; i++)
+          Flexible(flex: 0, child: _photoPlaceholder),
+      ],
+    );
+  }
+
+  Widget get _liveViewWithCounter {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        const LiveView(),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _getReadyText,
+            Flexible(
+              child: Container(
+                padding: const EdgeInsets.all(40.0),
+                constraints: const BoxConstraints(maxWidth: 600, maxHeight: 600),
+                child: Observer(builder: (_) {
+                  return AnimatedOpacity(
+                    duration: const Duration(milliseconds: 50),
+                    opacity: viewModel.showCounter ? 1.0 : 0.0,
+                    child: CaptureCounter(
+                      onCounterFinished: viewModel.onCounterFinished,
+                      counterStart: viewModel.counterStart,
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget get _photoPlaceholder {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: AspectRatio(
+        aspectRatio: 1.5,
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(
+            border: theme.captureCounterContainerBorder,
+          ),
+        ),
+      ),
     );
   }
 
@@ -110,13 +122,13 @@ class MultiCaptureScreenView extends ScreenViewBase<MultiCaptureScreenViewModel,
       child: SizedBox(
         height: 300,
         child: AnimatedTextKit(
-                pause: Duration(milliseconds: viewModel.counterStart >= 3 ? 1000 : 0),
-                isRepeatingAnimation: false,
-                animatedTexts: [
-                    RotateAnimatedText("Get Ready!", textStyle: theme.titleStyle, duration: const Duration(milliseconds: 1000)),
-                    RotateAnimatedText("Look at 📷", textStyle: theme.titleStyle, duration: const Duration(milliseconds: 1000)),
-                ],
-              ),
+          pause: Duration(milliseconds: viewModel.counterStart >= 3 ? 1000 : 0),
+          isRepeatingAnimation: false,
+          animatedTexts: [
+            RotateAnimatedText("Get Ready!", textStyle: theme.titleStyle, duration: const Duration(milliseconds: 1000)),
+            RotateAnimatedText("Look at 📷", textStyle: theme.titleStyle, duration: const Duration(milliseconds: 1000)),
+          ],
+        ),
       ),
     );
   }
