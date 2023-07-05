9c4a822b03b7c79ab414530d023b59b08760eb08
Sander in 't Hout <sander@inthout.me>
2023-07-06 01:03:15 +0200

    Possibly fix supporting libs environment variables not found by libgphoto2

---
+++ lib/utils/environment_variables.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: non_constant_identifier_names, camel_case_types
+
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:ffi/ffi.dart';
+
+final DynamicLibrary msvcrt = Platform.isWindows ? DynamicLibrary.open('msvcrt.dll') : DynamicLibrary.process();
+
+typedef _putenv_s_func = Int32 Function(Pointer<Utf8> e, Pointer<Utf8> v);
+typedef _putenv_s_func_dart = int Function(Pointer<Utf8> e, Pointer<Utf8> v);
+
+final _putenv_s = msvcrt.lookupFunction<_putenv_s_func, _putenv_s_func_dart>('_putenv_s');
+
+int putenv_s(String e, String v) {
+  return using((arena) {
+    return _putenv_s(e.toNativeUtf8(), v.toNativeUtf8());
+  });
+}
