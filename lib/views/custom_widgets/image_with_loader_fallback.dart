0661fd04d59194f255e6764eb247dee6d49e9b18
Sander in 't Hout <sander@inthout.me>
2024-03-23 13:22:08 +0100

    Fix ImageWithLoaderFallback state not being refreshed when image changed

--- lib/views/custom_widgets/image_with_loader_fallback.dart
+++ lib/views/custom_widgets/image_with_loader_fallback.dart
@@ -35,22 +35,35 @@ class _ImageWithLoaderFallbackState extends State<ImageWithLoaderFallback> {
   @override
   void initState() {
     super.initState();
+    _initializeFromWidget();
+  }
 
-    _imageWidget = widget._type == _Type.memory ?
-      Image.memory(
-        widget.bytes!,
-        frameBuilder: _frameBuilder,
-        fit: widget.fit,
-      ) :
-      Image.file(
-        widget.file!,
-        frameBuilder: _frameBuilder,
-        fit: widget.fit,
-      );
+  void _initializeFromWidget() {
+    _imageWidget = widget._type == _Type.memory
+        ? Image.memory(
+            widget.bytes!,
+            frameBuilder: _frameBuilder,
+            fit: widget.fit,
+          )
+        : Image.file(
+            widget.file!,
+            frameBuilder: _frameBuilder,
+            fit: widget.fit,
+          );
 
     _listener = ImageStreamListener((image, synchronousCall) => widget.decodeCallback?.call());
     _imageStream = _imageWidget.image.resolve(ImageConfiguration.empty);
     _imageStream.addListener(_listener);
+  }
+
+  @override
+  void didUpdateWidget(covariant ImageWithLoaderFallback oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.bytes != oldWidget.bytes || widget.file != oldWidget.file) {
+      _imageStream.removeListener(_listener);
+      _initializeFromWidget();
+    }
   }
 
   Widget _frameBuilder(BuildContext context, Widget child, int? frame, bool wasSynchronouslyLoaded) {
