f8a1be54a9bcbe8d76d4cd3ab97a189cfec2210f
Sander in 't Hout <sander@inthout.me>
2024-04-04 17:58:23 +0200

    Write MakerNote EXIF as UNDEFINED instead of STRING

--- rust/src/utils/jpeg.rs
+++ rust/src/utils/jpeg.rs
@@ -74,6 +74,7 @@ pub fn get_momento_booth_exif_tags_from_file(image_file_path: &str) -> Vec<Momen
     let exif_tags = exif.unwrap().get_values(TAGS);
 
     exif_tags.iter().map(|(tag, value)| {
+        println!("TAG FOUND: {:?} {:?}", tag, value);
         match tag {
             nom_exif::ExifTag::ImageDescription => MomentoBoothExifTag::ImageDescription(match value {
                 nom_exif::EntryValue::Text(value) => value.clone(),
@@ -107,7 +108,7 @@ impl From<MomentoBoothExifTag> for little_exif::exif_tag::ExifTag {
             MomentoBoothExifTag::Software(value) => little_exif::exif_tag::ExifTag::Software(value),
             MomentoBoothExifTag::CreateDate(value) => little_exif::exif_tag::ExifTag::CreateDate(value.format("%Y:%m:%d %H:%M:%S").to_string()),
             MomentoBoothExifTag::Orientation(value) => little_exif::exif_tag::ExifTag::Orientation(vec!(value as u16)),
-            MomentoBoothExifTag::MakerNote(value) => little_exif::exif_tag::ExifTag::UnknownSTRING(value, 0x927C, ExifTagGroup::ExifIFD),
+            MomentoBoothExifTag::MakerNote(value) => little_exif::exif_tag::ExifTag::UnknownUNDEF(value.as_bytes().to_vec(), 0x927C, ExifTagGroup::ExifIFD),
         }
     }
 }
@@ -132,9 +133,9 @@ impl TryFrom<&little_exif::exif_tag::ExifTag> for MomentoBoothExifTag {
                     None => Err("Unknown orientation"),
                 }
             },
-            little_exif::exif_tag::ExifTag::UnknownSTRING(value, tag_id, tag_group) => {
+            little_exif::exif_tag::ExifTag::UnknownUNDEF(value, tag_id, tag_group) => {
                 match (tag_id, tag_group) {
-                    (0x927C, ExifTagGroup::ExifIFD) => Ok(MomentoBoothExifTag::MakerNote(value.clone())),
+                    (0x927C, ExifTagGroup::ExifIFD) => Ok(MomentoBoothExifTag::MakerNote(String::from_utf8(value.to_vec()).unwrap())),
                     _ => Err("Unknown tag"),
                 }
             },
