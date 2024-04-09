18c9a338e0e8759d31a47a8e975d1357b5a6f61e
Casper Cromjongh <casper.cromjongh@hotmail.com>
2024-04-09 18:33:48 +0200

    Capture photo in the dialog.

--- lib/views/custom_widgets/dialogs/find_face_dialog.dart
+++ lib/views/custom_widgets/dialogs/find_face_dialog.dart
@@ -1,12 +1,16 @@
+import 'dart:io';
+
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:loggy/loggy.dart';
 import 'package:mobx/mobx.dart';
 import 'package:momento_booth/app_localizations.dart';
 import 'package:momento_booth/hardware_control/photo_capturing/live_view_stream_snapshot_capturer.dart';
 import 'package:momento_booth/hardware_control/photo_capturing/photo_capture_method.dart';
 import 'package:momento_booth/hardware_control/photo_capturing/sony_remote_photo_capture.dart';
 import 'package:momento_booth/managers/live_view_manager.dart';
+import 'package:momento_booth/managers/photos_manager.dart';
 import 'package:momento_booth/managers/settings_manager.dart';
 import 'package:momento_booth/models/settings.dart';
 import 'package:momento_booth/views/custom_widgets/buttons/photo_booth_filled_button.dart';
@@ -22,40 +26,57 @@ class FindFaceDialog extends StatefulWidget {
   final VoidCallback onCancel;
   final int countDown;
 
-  static const flashStartDuration = Duration(milliseconds: 50);
-  late final PhotoCaptureMethod capturer;
-  @computed
-  Duration get photoDelay => Duration(seconds: countDown) - capturer.captureDelay + flashStartDuration;
-
   FindFaceDialog({
     super.key,
     required this.title,
     required this.onDismiss,
     required this.onCancel,
     this.countDown = 3
-  }) {
-    capturer = switch (SettingsManager.instance.settings.hardware.captureMethod) {
-      CaptureMethod.sonyImagingEdgeDesktop => SonyRemotePhotoCapture(SettingsManager.instance.settings.hardware.captureLocation),
-      CaptureMethod.liveViewSource => LiveViewStreamSnapshotCapturer(),
-      CaptureMethod.gPhoto2 => LiveViewManager.instance.gPhoto2Camera!,
-    } as PhotoCaptureMethod;
-    // Future.delayed(photoDelay).then((_) => captureAndGetPhoto());
-  }
+  });
 
   @override
   State<FindFaceDialog> createState() => _FindFaceDialogState();
 }
 
-class _FindFaceDialogState extends State<FindFaceDialog> {
-  @observable
+class _FindFaceDialogState extends State<FindFaceDialog> with UiLoggy {
   bool showCounter = true;
+  bool captureComplete = false;
+
+  static const flashStartDuration = Duration(milliseconds: 50);
+  final PhotoCaptureMethod capturer = switch (SettingsManager.instance.settings.hardware.captureMethod) {
+    CaptureMethod.sonyImagingEdgeDesktop => SonyRemotePhotoCapture(SettingsManager.instance.settings.hardware.captureLocation),
+    CaptureMethod.liveViewSource => LiveViewStreamSnapshotCapturer(),
+    CaptureMethod.gPhoto2 => LiveViewManager.instance.gPhoto2Camera!,
+  } as PhotoCaptureMethod;
+  @computed
+  Duration get photoDelay => Duration(seconds: widget.countDown) - capturer.captureDelay + flashStartDuration;
+
+  Future<void> captureAndGetPhoto() async {
+    try {
+      final image = await capturer.captureAndGetPhoto();
+      PhotosManager.instance.photos.add(image);
+      PhotosManager.instance.outputImage = image.data;
+    } catch (error) {
+      loggy.warning(error);
+      final errorFile = File('assets/bitmap/capture-error.png');
+      PhotosManager.instance.outputImage = await errorFile.readAsBytes();
+    } finally {
+      captureComplete = true;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(photoDelay).then((_) => captureAndGetPhoto());
+  }
 
   void capture() {
     setState(() {
       showCounter = false;
     });
     print("Capture");
-    print("Method = ${widget.capturer}");
+    print("Method = ${capturer}");
   }
 
   @override
