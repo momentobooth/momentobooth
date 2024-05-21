271a9e35d08bed085855e80c5376266d80057a73
Sander in 't Hout <sander@inthout.me>
2024-05-21 21:58:40 +0200

    Fix HelperLibraryInitializationManager init, fix compile Additionally replace deprecated sound package as instructed by pub

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ void main() async {
     // Managers
     ..registerSingleton(HelperLibraryInitializationManager());
 
-  //await HelperLibraryInitializationManager.initialize();
+  await getIt<HelperLibraryInitializationManager>().initialize();
   await SettingsManager.instance.load();
   await StatsManager.instance.load();
   await WindowManager.instance.initialize();
