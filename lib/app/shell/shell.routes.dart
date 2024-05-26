6d899831b147e817f2bfbf4f4bdf7fca61da42e0
Sander in 't Hout <sander@inthout.me>
2024-05-27 00:51:07 +0200

    Add first bits of onboarding screen

--- lib/app/shell/shell.routes.dart
+++ lib/app/shell/shell.routes.dart
@@ -1,16 +1,28 @@
 part of 'shell.dart';
 
 List<GoRoute> _rootRoutes = [
+  _onboardingRoute,
   _photoBoothRoute,
   _settingsRoute,
 ];
+
+GoRoute _onboardingRoute = GoRoute(
+  path: "/onboarding",
+  pageBuilder: (context, state) {
+    return SettingsBasedTransitionPage.fromSettings(
+      key: state.pageKey,
+      enableTransitionOut: true,
+      child: const OnboardingPage(),
+    );
+  },
+);
 
 GoRoute _photoBoothRoute = GoRoute(
   path: "/photo_booth",
   pageBuilder: (context, state) {
     return SettingsBasedTransitionPage.fromSettings(
       key: state.pageKey,
-      enableTransitionOut: false,
+      enableTransitionOut: true,
       child: const PhotoBooth(),
     );
   },
@@ -18,7 +30,7 @@ GoRoute _photoBoothRoute = GoRoute(
 
 GoRoute _settingsRoute = GoRoute(
   path: "/settings",
-  pageBuilder: (context, state) { 
+  pageBuilder: (context, state) {
     return SettingsBasedTransitionPage.fromSettings(
       key: state.pageKey,
       opaque: false,
