0d6e2824377261172b9159f780a44732907ce3ef
Sander in 't Hout <sander@inthout.me>
2024-10-07 21:58:20 +0200

    Fix bug in iolibs and camlibs path resolving, add some logging

--- rust/src/helpers.rs
+++ rust/src/helpers.rs
@@ -16,12 +16,18 @@ pub fn initialize_hardware(iolibs_path: String, camlibs_path: String, ready_sink
     // gPhoto2 initialize
     if !iolibs_path.is_empty() && !camlibs_path.is_empty() {
         let mut full_iolibs_path = env::current_exe().unwrap();
+        full_iolibs_path.pop();
         full_iolibs_path.push(iolibs_path);
         env::set_var("IOLIBS", full_iolibs_path.to_str().unwrap());
 
         let mut full_camlibs_path = env::current_exe().unwrap();
+        full_camlibs_path.pop();
         full_camlibs_path.push(camlibs_path);
         env::set_var("CAMLIBS", full_camlibs_path.to_str().unwrap());
+
+        debug!("initialize_hardware(): iolibs: {}, camlibs: {}", full_iolibs_path.to_str().unwrap(), full_camlibs_path.to_str().unwrap());
+    } else {
+        debug!("{}", "initialize_hardware(): no override of iolibs or camlibs path");
     }
 
     let initialize_result = gphoto2::initialize();
