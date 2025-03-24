fb590145437f83e95a9495eb82cbf66531d73812
Sander in 't Hout <sander@inthout.me>
2025-03-24 22:40:02 +0100

    Add missing class modifiers to Freezed classes

--- lib/models/stats.dart
+++ lib/models/stats.dart
@@ -9,7 +9,7 @@ part 'stats.g.dart';
 // ///////////// //
 
 @freezed
-class Stats with _$Stats implements TomlEncodableValue {
+abstract class Stats with _$Stats implements TomlEncodableValue {
 
   const Stats._();
 
