abe138c18e7c4e10f757384a788cf4f6fcf180fd
Sander in 't Hout <sander@inthout.me>
2025-04-11 01:03:18 +0200

    Remove legacy photo booth theme

--- lib/extensions/build_context_extension.dart
+++ lib/extensions/build_context_extension.dart
@@ -1,12 +1,9 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:go_router/go_router.dart';
-import 'package:momento_booth/views/photo_booth_screen/theme/momento_booth_theme.dart';
-import 'package:momento_booth/views/photo_booth_screen/theme/momento_booth_theme_data.dart';
 import 'package:momento_booth/views/photo_booth_screen/theme/photo_booth_theme.dart';
 
 extension BuildContextExtension on BuildContext {
 
-  MomentoBoothThemeData get legacyTheme => MomentoBoothTheme.of(this).data;
   PhotoBoothTheme get theme => FluentTheme.of(this).photoBoothTheme;
   NavigatorState get navigator => Navigator.of(this);
   NavigatorState get rootNavigator => Navigator.of(this, rootNavigator: true);
