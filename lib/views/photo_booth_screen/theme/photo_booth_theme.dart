d60fe566066a762435069d49bd4e58092a03d750
Sander in 't Hout <sander@inthout.me>
2025-04-10 09:41:13 +0200

    Add theme as extension to Photo Booth Fluent theme, add basic theme as sample, port over some styles from legacy theme system

--- lib/views/photo_booth_screen/theme/photo_booth_theme.dart
+++ lib/views/photo_booth_screen/theme/photo_booth_theme.dart
@@ -4,13 +4,38 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:theme_tailor_annotation/theme_tailor_annotation.dart';
 
 part 'photo_booth_theme.tailor.dart';
+part 'photo_booth_theme.basic_theme.dart';
+part 'photo_booth_theme.wedding_theme.dart';
+
+typedef WidgetBuilder = Widget Function(BuildContext context, Widget child);
 
 @TailorMixin()
 class PhotoBoothTheme extends ThemeExtension<PhotoBoothTheme> with _$PhotoBoothThemeTailorMixin {
 
-  final PhotoBoothButtonTheme buttonTheme;
+  final TextTheme titleTheme;
+  final TextTheme subtitleTheme;
+  final PhotoBoothButtonTheme primaryButtonTheme;
+  final PhotoBoothButtonTheme navigationButtonTheme;
 
-  const PhotoBoothTheme({required this.buttonTheme});
+  const PhotoBoothTheme({
+    required this.titleTheme,
+    required this.subtitleTheme,
+    required this.primaryButtonTheme,
+    required this.navigationButtonTheme,
+  });
+
+  factory PhotoBoothTheme.defaultBasic() => _basicTheme;
+  //factory PhotoBoothTheme.defaultWedding() => _weddingTheme;
+
+}
+
+@TailorMixinComponent()
+class TextTheme extends ThemeExtension<TextTheme> with _$TextThemeTailorMixin {
+
+  final TextStyle style;
+  final WidgetBuilder? frameBuilder;
+
+  const TextTheme({required this.style, this.frameBuilder});
 
 }
 
@@ -18,8 +43,8 @@ class PhotoBoothTheme extends ThemeExtension<PhotoBoothTheme> with _$PhotoBoothT
 class PhotoBoothButtonTheme extends ThemeExtension<PhotoBoothButtonTheme> with _$PhotoBoothButtonThemeTailorMixin {
 
   final ButtonStyle style;
-  final Widget Function(BuildContext context, Widget child) frameBuilder;
+  final WidgetBuilder? frameBuilder;
 
-  const PhotoBoothButtonTheme({required this.style, required this.frameBuilder});
+  const PhotoBoothButtonTheme({required this.style, this.frameBuilder});
 
 }
