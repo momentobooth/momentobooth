4315e8c72967783c5495f0a31f105c47e7578cf2
Casper Cromjongh <casper.cromjongh@hotmail.com>
2025-05-21 11:54:54 +0200

    Add method to retrieve camera config.

--- lib/views/settings_overlay/settings_overlay_controller.dart
+++ lib/views/settings_overlay/settings_overlay_controller.dart
@@ -663,4 +663,9 @@ class SettingsOverlayController extends ScreenControllerBase<SettingsOverlayView
     logInfo("Folders: $folders");
   }
 
+  Future<void> onGetCameraConfigPressed() async {
+    final config = await getIt<LiveViewManager>().gPhoto2Camera!.getConfig();
+    logInfo("Config: $config");
+  }
+
 }
