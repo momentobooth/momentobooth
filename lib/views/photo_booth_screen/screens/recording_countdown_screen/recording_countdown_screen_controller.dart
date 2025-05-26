d6f962f3e734a92a98de9819da21549a1a746469
Casper Cromjongh <casper.cromjongh@hotmail.com>
2025-05-27 00:19:40 +0200

    Add video recording page.

---
+++ lib/views/photo_booth_screen/screens/recording_countdown_screen/recording_countdown_screen_controller.dart
@@ -0,0 +1,13 @@
+import 'package:momento_booth/views/base/screen_controller_base.dart';
+import 'package:momento_booth/views/photo_booth_screen/screens/recording_countdown_screen/recording_countdown_screen_view_model.dart';
+
+class RecordingCountdownScreenController extends ScreenControllerBase<RecordingCountdownScreenViewModel> {
+
+  // Initialization/Deinitialization
+
+  RecordingCountdownScreenController({
+    required super.viewModel,
+    required super.contextAccessor,
+  });
+
+}
