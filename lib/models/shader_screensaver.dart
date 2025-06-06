2c4e19d8b54cc9c7cfa7f601bae0565c6790348d
Sander in 't Hout <sander@inthout.me>
2025-06-06 22:29:32 +0200

    Improve Not Available screen, add settings for toggling availability and screensaver

--- lib/models/shader_screensaver.dart
+++ lib/models/shader_screensaver.dart
@@ -5,12 +5,14 @@ part 'shader_screensaver.g.dart';
 
 @freezed
 abstract class ShaderScreensaver with _$ShaderScreensaver {
+
   const factory ShaderScreensaver({
-    String title,
-    String author,
-    String sourceUrl,
-    String screensaverTimeDilation,
+    required String title,
+    required String author,
+    required String sourceUrl,
+    required String screensaverTimeDilation,
   }) = _ShaderScreensaver;
 
   factory ShaderScreensaver.fromJson(Map<String, Object?> json) => _$ShaderScreensaverFromJson(json);
+
 }
