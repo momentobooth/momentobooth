6fca01609e369f0a7ecf76fdcf78ca717d557389
Casper Cromjongh <casper.cromjongh@hotmail.com>
2025-06-08 13:06:22 +0200

    Fix rebase issues.
    Mostly theming related and some argument changes.

--- lib/app.routes.dart
+++ lib/app.routes.dart
@@ -76,7 +76,7 @@ GoRoute _multiCaptureRoute = GoRoute(
 GoRoute _recordingRoute = GoRoute(
   path: RecordingCountdownScreen.defaultRoute,
   pageBuilder: (context, state) {
-    return TransitionPage.fromSettings(key: state.pageKey, child: const RecordingCountdownScreen());
+    return TransitionPage.fromSettings(key: state.pageKey, context: context, child: const RecordingCountdownScreen());
   },
 );
 
