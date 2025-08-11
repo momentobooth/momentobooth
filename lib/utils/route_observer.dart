afe9b927333c3a89aeade0e7781a1f7e2dce1f24
Sander in 't Hout <sander@inthout.me>
2025-08-11 22:12:02 +0200

    Fix incorrect route logged and reported to MQTT

--- lib/utils/route_observer.dart
+++ lib/utils/route_observer.dart
@@ -8,62 +8,33 @@ class GoRouterObserver extends NavigatorObserver with Logger {
 
   @override
   void didPush(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    if (route.settings is CustomTransitionPage) {
-      CustomTransitionPage page = route.settings as CustomTransitionPage;
-      String routeType = page.child.runtimeType.toString();
-
-      logDebug("Route push: $routeType");
-      getIt<MqttManager>().publishScreen(routeType);
-    } else {
-      logDebug("Route push: Unknown (is not a CustomTransitionPage))");
-    }
+    String routeName = route.settings.name ?? 'Unknown';
+    logDebug("Route push: $routeName");
+    getIt<MqttManager>().publishScreen(routeName);
   }
 
   @override
   void didPop(Route route, Route? previousRoute) {
-    if (route.settings is CustomTransitionPage) {
-      CustomTransitionPage page = route.settings as CustomTransitionPage;
-      String routeType = page.child.runtimeType.toString();
-
-      logDebug("Route pop: $routeType");
-      getIt<MqttManager>().publishScreen(routeType);
-    } else {
-      logDebug("Route pop: Unknown (is not a CustomTransitionPage))");
-    }
+    String routeName = route.settings.name ?? 'Unknown';
+    String previousRouteName = previousRoute == null ? 'None' : previousRoute.settings.name ?? 'Unknown';
+    logDebug("Route pop: $routeName, Previous: $previousRouteName");
+    getIt<MqttManager>().publishScreen(previousRouteName);
   }
 
   @override
   void didRemove(Route route, Route? previousRoute) {
-    if (route.settings is CustomTransitionPage) {
-      CustomTransitionPage page = route.settings as CustomTransitionPage;
-      String routeType = page.child.runtimeType.toString();
-
-      logDebug("Route remove: $routeType");
-    } else {
-      logDebug("Route remove: Unknown (is not a CustomTransitionPage))");
-    }
+    String routeName = route.settings.name ?? 'Unknown';
+    String previousRouteName = previousRoute == null ? 'None' : previousRoute.settings.name ?? 'Unknown';
+    logDebug("Route remove: $routeName, Previous: $previousRouteName");
   }
 
   @override
   void didReplace({Route? newRoute, Route? oldRoute}) {
-    String? newRouteChildName, oldRouteChildName;
+    String newRouteName = newRoute == null ? 'None' : newRoute.settings.name ?? 'Unknown';
+    String oldRouteeName = oldRoute == null ? 'None' : oldRoute.settings.name ?? 'Unknown';
 
-    if (newRoute == null) {
-      newRouteChildName = "None";
-    } else if (newRoute.settings is CustomTransitionPage) {
-      CustomTransitionPage page = newRoute.settings as CustomTransitionPage;
-      newRouteChildName = page.child.runtimeType.toString();
-    }
-
-    if (oldRoute == null) {
-      oldRouteChildName = "None";
-    } else if (oldRoute.settings is CustomTransitionPage) {
-      CustomTransitionPage page = oldRoute.settings as CustomTransitionPage;
-      oldRouteChildName = page.child.runtimeType.toString();
-    }
-
-    logDebug("Route replaced ${oldRouteChildName ?? 'Unknown (is not a CustomTransitionPage)'} with ${newRouteChildName ?? 'Unknown (is not a CustomTransitionPage)'}");
-    if (newRouteChildName != null) getIt<MqttManager>().publishScreen(newRouteChildName);
+    logDebug("Route replaced $oldRouteeName with $newRouteName");
+    getIt<MqttManager>().publishScreen(newRouteName);
   }
 
 }
