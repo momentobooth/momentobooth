2bd68f8b4476c6fb2a11e957759814b19e8d96dc
Casper Cromjongh <casper.cromjongh@hotmail.com>
2025-08-21 11:40:29 +0200

    Forgot to remove the import.

--- lib/views/settings_overlay/settings_overlay_controller.dart
+++ lib/views/settings_overlay/settings_overlay_controller.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:momento_booth/main.dart';
-import 'package:momento_booth/managers/external_system_status_manager.dart';
 import 'package:momento_booth/managers/photos_manager.dart';
 import 'package:momento_booth/managers/sfx_manager.dart';
 import 'package:momento_booth/models/maker_note_data.dart';
