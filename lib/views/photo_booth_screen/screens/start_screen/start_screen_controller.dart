49b30c12f413f3699dc4235732bdf4336b403d28
Casper Cromjongh <casper.cromjongh@hotmail.com>
2025-09-12 12:08:02 +0200

    Go directly to capture mode if only one is selected.

--- lib/views/photo_booth_screen/screens/start_screen/start_screen_controller.dart
+++ lib/views/photo_booth_screen/screens/start_screen/start_screen_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter/widgets.dart';
 import 'package:momento_booth/main.dart';
 import 'package:momento_booth/managers/project_manager.dart';
@@ -8,6 +9,8 @@ import 'package:momento_booth/views/base/screen_controller_base.dart';
 import 'package:momento_booth/views/components/dialogs/no_project_open_dialog.dart';
 import 'package:momento_booth/views/photo_booth_screen/screens/choose_capture_mode_screen/choose_capture_mode_screen.dart';
 import 'package:momento_booth/views/photo_booth_screen/screens/gallery_screen/gallery_screen.dart';
+import 'package:momento_booth/views/photo_booth_screen/screens/multi_capture_screen/multi_capture_screen.dart';
+import 'package:momento_booth/views/photo_booth_screen/screens/single_capture_screen/single_capture_screen.dart';
 import 'package:momento_booth/views/photo_booth_screen/screens/start_screen/start_screen_view_model.dart';
 
 class StartScreenController extends ScreenControllerBase<StartScreenViewModel> with PrinterStatusDialogMixin<StartScreenViewModel> {
@@ -32,7 +35,32 @@ class StartScreenController extends ScreenControllerBase<StartScreenViewModel> w
   // User interaction methods
 
   Future<void> onPressedContinue() async {
-    router.go(ChooseCaptureModeScreen.defaultRoute);
+    final singleCapture = getIt<ProjectManager>().settings.enableSingleCapture;
+    final collageCapture = getIt<ProjectManager>().settings.enableCollageCapture;
+    if (singleCapture && collageCapture) {
+      router.go(ChooseCaptureModeScreen.defaultRoute);
+      return;
+    } else if (singleCapture) {
+      router.go(SingleCaptureScreen.defaultRoute);
+      return;
+    } else if (collageCapture) {
+      router.go(MultiCaptureScreen.defaultRoute);
+      return;
+    } else {
+      // This should never happen, but just in case
+      await showUserDialog(
+        dialog: ContentDialog(
+          title: const Text("No capture modes enabled"),
+          content: const Text("No capture modes are enabled in the project settings. Please enable at least one capture mode to continue."),
+          actions: [
+            Button(
+              child: const Text("OK"),
+              onPressed: () { navigator.pop(); },
+            ),
+          ],
+        ), barrierDismissible: false,
+      );
+    }
   }
 
   void onPressedGallery() {
