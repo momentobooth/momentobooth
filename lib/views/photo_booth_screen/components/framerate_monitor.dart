5a07458f46ba7aeb94e7190cb3c3669db28c6d40
Sander in 't Hout <sander@inthout.me>
2026-02-15 11:34:05 +0100

    Fix settings overlay duplication caused by SetScrollConfiguration and FramerateMonitor

--- lib/views/photo_booth_screen/components/framerate_monitor.dart
+++ lib/views/photo_booth_screen/components/framerate_monitor.dart
@@ -4,22 +4,33 @@ import 'package:lemberfpsmonitor/lemberfpsmonitor.dart';
 import 'package:momento_booth/main.dart';
 import 'package:momento_booth/managers/settings_manager.dart';
 
-class FramerateMonitor extends StatelessWidget {
+class FramerateMonitor extends StatefulWidget {
 
   final Widget child;
 
   const FramerateMonitor({super.key, required this.child});
 
   @override
+  State<FramerateMonitor> createState() => _FramerateMonitorState();
+
+}
+
+class _FramerateMonitorState extends State<FramerateMonitor> {
+
+  // Without keying the child, all subtree state get's lost on toggling this,
+  // causing duplicate Settings screen on top of each other...
+  final GlobalKey _childKey = GlobalKey();
+
+  @override
   Widget build(BuildContext context) {
     return Observer(builder: (_) {
-      if (!getIt<SettingsManager>().settings.debug.showFpsCounter) return child;
+      if (!getIt<SettingsManager>().settings.debug.showFpsCounter) return KeyedSubtree(key: _childKey, child: widget.child);
 
       return FPSMonitor(
         showFPSChart: true,
         align: Alignment.topRight,
         onFPSChanged: (fps) {},
-        child: child,
+        child: KeyedSubtree(key: _childKey, child: widget.child),
       );
     });
   }
