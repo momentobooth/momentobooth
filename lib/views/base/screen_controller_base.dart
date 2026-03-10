c74dd513c55b081467d7bc725e4aad1fb57e30a1
Casper Cromjongh <casper.cromjongh@hotmail.com>
2026-03-10 14:17:53 +0100

    Attempt adding actions to modals based on their buttons.

--- lib/views/base/screen_controller_base.dart
+++ lib/views/base/screen_controller_base.dart
@@ -1,13 +1,11 @@
 import 'package:meta/meta.dart';
-import 'package:momento_booth/main.dart';
-import 'package:momento_booth/managers/_all.dart';
-import 'package:momento_booth/models/app_action.dart';
 import 'package:momento_booth/utils/logger.dart';
 import 'package:momento_booth/views/base/build_context_abstractor.dart';
 import 'package:momento_booth/views/base/build_context_accessor.dart';
+import 'package:momento_booth/views/base/has_actions_mixin.dart';
 import 'package:momento_booth/views/base/screen_view_model_base.dart';
 
-abstract class ScreenControllerBase<T extends ScreenViewModelBase> with BuildContextAbstractor, Logger {
+abstract class ScreenControllerBase<T extends ScreenViewModelBase> with BuildContextAbstractor, Logger, HasActionsMixin {
 
   final T viewModel;
 
@@ -18,15 +16,12 @@ abstract class ScreenControllerBase<T extends ScreenViewModelBase> with BuildCon
     required this.viewModel,
     required this.contextAccessor,
   }) {
-    getIt<ActionManager>().push(actions, _actionStackToken);
+    pushActions();
   }
 
   @mustCallSuper
   void dispose() {
-    getIt<ActionManager>().pop(_actionStackToken);
+    popActions();
   }
-
-  final Object _actionStackToken = Object();
-  List<AppAction> get actions => [];
 
 }
